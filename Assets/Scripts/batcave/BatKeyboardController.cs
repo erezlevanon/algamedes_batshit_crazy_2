bfc3925953dc4427d29ba8dfdd93d6c1c822eaa9
Erez Levanon <erez.leavnon@mail.huji.ac.il>
2017-11-18 17:22:03 +0200

    bat controller

---
+++ Assets/Scripts/batcave/BatKeyboardController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace BatCave {
+	/// <summary>
+	/// A scriptable object that allows getting control from several bat controllers.
+	/// </summary>
+	[CreateAssetMenu(menuName = "Bat Controller/Keyboard")]
+
+	public class BatKeyboardController : BatController {
+
+		private bool wantsToFlyUp;
+
+		/// <summary>
+		/// Returns true if any of the controllers wants the bat to fly up.
+		/// </summary>
+		public override bool WantsToFlyUp() {
+			// Handle keyboard input.
+			if (Input.GetKeyDown(KeyCode.Space)) {
+				wantsToFlyUp = true;
+			} else if (Input.GetKeyUp(KeyCode.Space)) {
+				wantsToFlyUp = false;
+			}
+			return wantsToFlyUp;
+		}
+}
+}
---
+++ Assets/Scripts/batcave/BatMouseController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using Infra.Gameplay.UI;
+
+namespace BatCave {
+	/// <summary>
+	/// A scriptable object that allows getting control from several bat controllers.
+	/// </summary>
+	[CreateAssetMenu(menuName = "Bat Controller/Mouse")]
+
+	public class BatMouseController : BatController {
+
+		private bool wantsToFlyUp;
+
+		/// <summary>
+		/// Returns true if any of the controllers wants the bat to fly up.
+		/// </summary>
+		public override bool WantsToFlyUp() {
+			return wantsToFlyUp;
+		}
+
+		protected void OnEnable() {
+			wantsToFlyUp = false;
+			GameInputCapture.OnTouchDown += OnTouchDown;
+			GameInputCapture.OnTouchUp += OnTouchUp;
+		}
+
+		protected void OnDisable() {
+			GameInputCapture.OnTouchDown -= OnTouchDown;
+			GameInputCapture.OnTouchUp -= OnTouchUp;
+		}
+
+		private void OnTouchDown(PointerEventData e) {
+			// Check that the event was not already handled by the GameManager.
+			if (e.used) return;
+			wantsToFlyUp = true;
+		}
+
+		private void OnTouchUp(PointerEventData e) {
+			wantsToFlyUp = false;
+		}
+}
+}
